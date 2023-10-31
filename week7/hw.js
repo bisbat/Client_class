3167bbb189ddad7ac79197c08294f650d7b35be2
bisbat <110973307+bisbat@users.noreply.github.com>
2023-10-31 10:35:03 +0700

    Update hw.js

--- week7/hw.js
+++ week7/hw.js
@@ -5,3 +5,18 @@ const persons = [
 ]
 //return sorted array by fullname (ascending order and ignore case)
 //[ { id: 2, fullname: 'ann Smith' },   { id: 3, fullname: 'John Lee' }, { id: 1, fullname: 'Susan Jo' },]
+
+persons.sort((p1,p2)=>{
+    const nameA=p1.fullname.toLowerCase()
+    const nameB=p2.fullname.toLowerCase()
+
+    if(nameA<nameB){
+      return -1
+    }else if(nameA>nameB){
+      return 1
+    }else{
+      return 0
+    }
+  }
+)
+console.log(persons);
